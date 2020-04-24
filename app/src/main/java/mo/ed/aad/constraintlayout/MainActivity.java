260fde65a4d1b2ae84f24220beba21389b85bec7
Prof-Mohamed Atef <proofesser@gmail.com>
2020-04-24 13:55:02 +0200

    2. Basic Features of ConstraintLayout - Constraints and Bias

---
+++ app/src/main/java/mo/ed/aad/constraintlayout/MainActivity.java
@@ -0,0 +1,16 @@
+package mo.ed.aad.constraintlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import mo.ed.aad.constraintlayout.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
